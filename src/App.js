02ba23619acdd2fd58a98d6806396a8a0b846679
wacoo <wabaham9@gmail.com>
2024-02-08 14:32:47 +0300

    Material ui react first page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import SearchAppBar from './components/AppBar';
+import TourCard from './components/TourCard';
+import { Container, Grid, Typography } from '@mui/material';
+import cities from './data.json';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchAppBar />
+      <Container sx={{my: 5}}>
+      { cities.map((city) => (
+        <>
+        <Typography variant='h4' component='h2' marginTop={5} marginBottom={3}>
+          Top {city.name} Tours 
+        </Typography>
+        <Grid container spacing={5}>
+          { city.tours.map((tour, index) => (
+              <TourCard tour={tour} key={index}/>
+            ))
+          }
+        </Grid>
+        </>
+      ))
+      }
+        
+      </Container>
     </div>
   );
 }
---
+++ src/components/TourCard.jsx
@@ -0,0 +1,78 @@
+import { Box, Grid, Rating, Typography } from '@mui/material';
+import Paper from '@mui/material/Paper';
+import { AccessTime } from '@mui/icons-material';
+import { createTheme, ThemeProvider } from '@mui/material';
+
+const theme = createTheme({
+    components: {
+        MuiTypography: {
+            variants:[
+                {
+                    props: {
+                        variant: 'body2'
+                    },
+                    style: {
+                        fontSize: 11,
+                    }
+                },
+                {
+                    props: {
+                        variant: 'body3'
+                    },
+                    style: {
+                        fontSize: 9,
+                        color: 'red',
+                    }
+                }
+            ]
+        }
+    }
+})
+const TourCard = ({tour}) => {
+    return (
+        <Grid item xs={3}>
+            <ThemeProvider theme={theme}>
+            <Paper elevation={3}>
+                <img className="img" src={tour.image} alt="Niagra" />
+                <Box paddingX={1}>
+                    <Typography variant='subtitle1' component='h2'>
+                        {tour.name}
+                    </Typography>
+                    <Box sx={{
+                        display: 'flex',
+                        alignItems: 'center'
+                    }}>
+                        <AccessTime sx={{ width: 12.5 }} />
+                        <Typography variant='body2' component='p' marginLeft={0.5}>
+                            {tour.duration} hours
+                        </Typography>
+                    </Box>
+                    <Box sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                    }}
+                    marginTop={3}>
+                            <Rating name='read-only' value={tour.rating} readOnly precision={0.5} size='small'/>
+                            <Typography variant='body2' component='p' marginLeft={0.5}>
+                                {tour.rating}
+                            </Typography>
+                            <Typography variant='body3' component='p' marginLeft={1.5}>
+                                ({tour.numberOfReviews} reviews)
+                            </Typography>
+                    </Box>
+                    
+                    <Box>
+                        <Typography variant='h6' component='h3' marginTop={0}>
+                            From C ${tour.price}
+                        </Typography>
+                    </Box>
+                </Box>
+            </Paper>
+            </ThemeProvider>
+            
+        </Grid>
+    );
+
+}
+
+export default TourCard;
